f5760fb25d5c4035f13dbcf9ba8162bd5d3d7894
Ted Wei <jdwei@hotmail.com>
2017-02-07 06:24:18 +0800

    merge NinjiaMode

--- App/AppDelegate.m
+++ App/AppDelegate.m
@@ -60,6 +60,8 @@
 
 @property (nonatomic, assign, readwrite) IBOutlet NSWindow *    window;
 @property (nonatomic, assign, readwrite) IBOutlet NSTextView *  textView;
+@property (strong, nonatomic) NSStatusItem *statusItem;
+@property (assign, nonatomic) BOOL darkModeOn;
 
 - (IBAction)installAction:(id)sender;
 - (IBAction)getVersionAction:(id)sender;
@@ -109,6 +111,19 @@
     }
     
     [self.window makeKeyAndOrderFront:self];
+    
+    // NinjiaMode
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    _statusItem.image = [NSImage imageNamed:@"switchIcon.png"];
+    [_statusItem.image setTemplate:YES];
+    
+    _statusItem.highlightMode = NO;
+    _statusItem.toolTip = @"control-click to quit";
+    
+    [_statusItem setAction:@selector(itemClicked:)];
+    
+    [self refreshDarkMode];
+    [self turnInternetOff];
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
@@ -317,5 +332,62 @@
         }
     }];
 }
+
+- (void)refreshDarkMode {
+    NSString * value = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
+    if ([value isEqualToString:@"Dark"]) {
+        self.darkModeOn = YES;
+    }
+    else {
+        self.darkModeOn = NO;
+    }
+}
+
+- (void)itemClicked:(id)sender {
+    //Look for control click, close app if so
+    NSEvent *event = [NSApp currentEvent];
+    if([event modifierFlags] & NSControlKeyMask) {
+        [[NSApplication sharedApplication] terminate:self];
+        return;
+    }
+    
+    //Change theme
+    [self toggleTheme];
+    
+    //Toggle darkMode
+    _darkModeOn = !_darkModeOn;
+    
+    //Change desktop
+    if (_darkModeOn) {
+        [self turnInternetOn];
+    }
+    else {
+        [self turnInternetOff];
+    }
+}
+
+- (void)toggleTheme {
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"ThemeToggle" ofType:@"scpt"];
+    NSURL* url = [NSURL fileURLWithPath:path];
+    NSDictionary* errors = [NSDictionary dictionary];
+    NSAppleScript* appleScript = [[NSAppleScript alloc] initWithContentsOfURL:url error:&errors];
+    [appleScript executeAndReturnError:nil];
+}
+
+- (void)turnInternetOff {
+//    system("networksetup -setwebproxy Wi-Fi 192.168.1.100");
+//    system("networksetup -setsecurewebproxy Wi-Fi 192.168.1.100");
+}
+
+- (void)turnInternetOn {
+//    system("networksetup -setwebproxystate Wi-Fi off");
+//    system("networksetup -setsecurewebproxystate Wi-Fi off");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    //Attempt to restore things back the way we found them
+    [self turnInternetOn];
+}
+
 
 @end
